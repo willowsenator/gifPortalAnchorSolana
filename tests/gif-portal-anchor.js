149f71ca475cd56319c5692daec3b73b5d68a566
willowsenator <willowsenator@gmail.com>
2022-12-12 00:43:53 +0100

    Add new function to add Gif info to blockchain

--- tests/gif-portal-anchor.js
+++ tests/gif-portal-anchor.js
@@ -1,10 +1,34 @@
 const anchor = require('@project-serum/anchor')
 const main = async() =>{
   console.log('Starting tests')
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.GifPortalAnchor;
-  const tx = await program.rpc.startStuffOff()
+  const baseAccount = anchor.web3.Keypair.generate();
+  const tx = await program.rpc.startStuffOff(
+      {
+        accounts:{
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers:[baseAccount]
+      }
+  )
   console.log('Transaction signature: ', tx)
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey)
+  console.log("GIF count: ", account.totalGifs.toString())
+
+  await program.rpc.addGif("https://media0.giphy.com/media/3o7TKrEzvLbsVAud8I/giphy.gif",{
+    accounts:{
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    }
+  })
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey)
+  console.log("GIF count: ", account.totalGifs.toString())
+  console.log("GIF list: ", account.gifList)
 }
 
 const runMain = async() =>{
